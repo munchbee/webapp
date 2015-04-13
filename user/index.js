59443b8781580b1d156d117fed6ceda7352b91f7
ishaan.bansal <ishaan.bansal.29@gmail.com>
2015-04-13 21:27:45 +0530

    added user schema

---
+++ user/index.js
@@ -0,0 +1,32 @@
+var User = function () {
+	this.data = {
+		userID : null,
+		firstName : null,
+		lastName : null,
+		emailID : null,
+		password : null,
+		contactNumber : null,
+		company : null,
+		orderID : null
+	};
+
+	this.fill = function (info) {
+		for(var prop in this.data) {
+			if(this.data[prop] !== 'undefined') {
+				this.data[prop] = info[prop];
+			}
+		}
+	};
+
+	this.getInformation = function () {
+		return this.data;
+	};
+};
+
+module.exports = function (info) {
+	var instance = new User();
+
+	instance.fill(info);
+
+	return instance;
+};
